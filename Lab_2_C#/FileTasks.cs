1774a761f74df09a68c5a16dc3e3bd0b36a284a0
Pank-cake <od.uvanchik@yandex.ru>
2026-05-13 00:48:58 +0500

    FileTasks.cs

--- Lab_2_C#/FileTasks.cs
+++ Lab_2_C#/FileTasks.cs
@@ -1,270 +1,237 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
 
-namespace lab7
+namespace Lab7
 {
+    public struct Toy
+    {
+        private string _name;
+        private int _price;
+        private int _minAge;
+        private int _maxAge;
+
+        public string Name
+        {
+            get 
+            { 
+                return _name; 
+            }
+            set 
+            { 
+                _name = value; 
+            }
+        }
+
+        public int Price
+        {
+            get 
+            { 
+                return _price; 
+            }
+            set 
+            { 
+                _price = value; 
+            }
+        }
+
+        public int MinAge
+        {
+            get 
+            { 
+                return _minAge; 
+            }
+            set 
+            { 
+                _minAge = value; 
+            }
+        }
+
+        public int MaxAge
+        {
+            get 
+            { 
+                return _maxAge; 
+            }
+            set 
+            { 
+                _maxAge = value; 
+            }
+        }
+    }
+
     public static class FileTasks
     {
-        private static Random rnd = new Random();
+        private static Random _random = new Random();
 
-        private static void FillFileOneNumberPerLine(string filePath, int count)
+        public static void CreateTask1File(string path)
         {
-            using (StreamWriter writer = new StreamWriter(filePath))
+            using (StreamWriter writer = new StreamWriter(path))
             {
-                for (int i = 0; i < count; i++)
-                    writer.WriteLine(rnd.Next(-100, 101));
-            }
-        }
-
-        private static void FillFileMultipleNumbersPerLine(string filePath, int linesCount)
-        {
-            using (StreamWriter writer = new StreamWriter(filePath))
-            {
-                for (int i = 0; i < linesCount; i++)
+                for (int i = 0; i < 10; i++)
                 {
-                    int numbersInLine = rnd.Next(3, 7);
-                    for (int j = 0; j < numbersInLine; j++)
-                        writer.Write(rnd.Next(-50, 51) + " ");
-                    writer.WriteLine();
+                    writer.WriteLine(_random.Next(-100, 101));
                 }
-            }
-        }
-
-        private static void FillTextFileTask3(string filePath)
-        {
-            Console.WriteLine("Введите текст построчно (пустая строка — конец ввода):\n");
-
-            List<string> lines = new List<string>();
-            int lineNumber = 1;
-
-            string firstLine = InputValidator.ReadNonEmptyLine($"Строка {lineNumber}: ");
-            lines.Add(firstLine);
-            lineNumber++;
-
-            while (true)
-            {
-                Console.Write($"Строка {lineNumber}: ");
-                string input = Console.ReadLine();
-
-                if (string.IsNullOrWhiteSpace(input)) break;
-
-                lines.Add(input);
-                lineNumber++;
-            }
-
-            using (StreamWriter writer = new StreamWriter(filePath))
-            {
-                for (int i = 0; i < lines.Count; i++)
-                    writer.WriteLine(lines[i]);
-            }
-
-            Console.WriteLine($"\nСохранено {lines.Count} строк в '{filePath}'.\n");
-        }
-
-        private static void FillBinaryFile(string filePath, int count)
-        {
-            using (BinaryWriter writer = new BinaryWriter(File.OpenWrite(filePath)))
-            {
-                for (int i = 0; i < count; i++)
-                    writer.Write(rnd.Next(1, 100));
-            }
-        }
-
-        private static void FillToysFile(string filePath)
-        {
-            List<Toy> toys = new List<Toy>();
-            string[] names = { "мяч", "конструктор", "кукла", "машинка", "пазл", "робот", "мишка", "юла" };
-
-            for (int i = 0; i < 6; i++)
-            {
-                Toy toy = new Toy();
-                toy.Name = names[rnd.Next(names.Length)];
-                toy.Price = rnd.Next(100, 1000);
-                toy.MinAge = rnd.Next(0, 8);
-                toy.MaxAge = toy.MinAge + rnd.Next(1, 8);
-                toys.Add(toy);
-            }
-
-            XmlSerializer xml = new XmlSerializer(typeof(List<Toy>));
-            using (FileStream fs = new FileStream(filePath, FileMode.Create))
-            {
-                xml.Serialize(fs, toys);
             }
         }
 
         public static void SolveTask1()
         {
-            Console.Clear();
-            Console.WriteLine("Задание 1. Сумма max и min\n");
+            string path = "task1.txt";
+            CreateTask1File(path);
 
-            string filePath = "task1.txt";
-            FillFileOneNumberPerLine(filePath, 12);
+            string[] lines = File.ReadAllLines(path);
+            int min = int.Parse(lines[0]);
+            int max = int.Parse(lines[0]);
 
-            string[] lines = File.ReadAllLines(filePath);
-            Console.WriteLine($"Файл '{filePath}' заполнен случайными числами ({lines.Length} шт.):");
-
-            int min = int.MaxValue;
-            int max = int.MinValue;
-
-            for (int i = 0; i < lines.Length; i++)
+            foreach (string line in lines)
             {
-                int num = int.Parse(lines[i].Trim());
-                Console.Write(num + " ");
-                if (num < min) min = num;
-                if (num > max) max = num;
+                int number = int.Parse(line);
+                if (number < min) min = number;
+                if (number > max) max = number;
             }
 
-            Console.WriteLine($"\n\nМинимум: {min}");
-            Console.WriteLine($"Максимум: {max}");
-            Console.WriteLine($"Сумма: {min + max}");
-            Console.Write("\nНажмите любую клавишу...");
-            Console.ReadKey();
+            Console.WriteLine("Содержимое файла: " + string.Join(", ", lines));
+            Console.WriteLine("Результат Min + Max: " + (min + max));
+        }
+
+        public static void CreateTask2File(string path)
+        {
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                writer.WriteLine(_random.Next(1, 50) + " " + _random.Next(1, 50));
+                writer.WriteLine(_random.Next(1, 50) + " " + _random.Next(1, 50));
+            }
         }
 
         public static void SolveTask2()
         {
-            Console.Clear();
-            Console.WriteLine("Задание 2. Сумма чётных\n");
+            string path = "task2.txt";
+            CreateTask2File(path);
 
-            string filePath = "task2.txt";
-            FillFileMultipleNumbersPerLine(filePath, 6);
+            string[] lines = File.ReadAllLines(path);
+            int evenSum = 0;
 
-            string[] lines = File.ReadAllLines(filePath);
-            Console.WriteLine($"Файл '{filePath}' заполнен случайными числами:\n");
-
-            int sum = 0;
-
-            for (int i = 0; i < lines.Length; i++)
+            foreach (string line in lines)
             {
-                Console.WriteLine("  " + lines[i]);
-                string[] tokens = lines[i].Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
-                for (int j = 0; j < tokens.Length; j++)
+                string[] parts = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string part in parts)
                 {
-                    int num = int.Parse(tokens[j]);
-                    if (num % 2 == 0) sum += num;
+                    int number = int.Parse(part);
+                    if (number % 2 == 0) evenSum += number;
                 }
             }
-
-            Console.WriteLine($"\nСумма чётных: {sum}");
-            Console.Write("\nНажмите любую клавишу...");
-            Console.ReadKey();
+            Console.WriteLine("Файл:\n" + File.ReadAllText(path));
+            Console.WriteLine("Сумма четных чисел: " + evenSum);
         }
 
         public static void SolveTask3()
         {
-            Console.Clear();
-            Console.WriteLine("Задание 3. Первые символы строк\n");
-
             string inputPath = "task3_input.txt";
             string outputPath = "task3_output.txt";
 
-            FillTextFileTask3(inputPath);
-
-            string[] lines = File.ReadAllLines(inputPath);
-
-            using (StreamWriter writer = new StreamWriter(outputPath))
+            Console.WriteLine("Введите текст (пустая строка для завершения):");
+            List<string> userLines = new List<string>();
+            while (true)
             {
-                for (int i = 0; i < lines.Length; i++)
+                string input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input)) break;
+                userLines.Add(input);
+            }
+
+            File.WriteAllLines(inputPath, userLines);
+            List<string> firstCharacters = new List<string>();
+
+            foreach (string line in File.ReadAllLines(inputPath))
+            {
+                if (line.Length > 0)
                 {
-                    if (lines[i].Length > 0)
-                        writer.WriteLine(lines[i][0]);
-                    else
-                        writer.WriteLine();
+                    firstCharacters.Add(line[0].ToString());
                 }
             }
 
-            Console.WriteLine($"Файл '{outputPath}' создан. Первые символы строк:");
-            string[] outLines = File.ReadAllLines(outputPath);
-            for (int i = 0; i < outLines.Length; i++)
-                Console.WriteLine($"  '{outLines[i]}'");
+            File.WriteAllLines(outputPath, firstCharacters);
+            Console.WriteLine("Первые символы сохранены в " + outputPath);
+        }
 
-            Console.Write("\nНажмите любую клавишу...");
-            Console.ReadKey();
+        public static void CreateTask4File(string path)
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Create)))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    writer.Write(_random.Next(1, 101));
+                }
+            }
         }
 
         public static void SolveTask4()
         {
-            Console.Clear();
-            Console.WriteLine("Задание 4. Исключить кратные k\n");
+            string inputPath = "task4.bin";
+            string outputPath = "task4_result.bin";
+            CreateTask4File(inputPath);
 
-            string inputPath = "task4_input.bin";
-            string outputPath = "task4_output.bin";
-
-            FillBinaryFile(inputPath, 15);
-            Console.WriteLine($"Файл '{inputPath}' заполнен случайными числами (15 шт.).");
-
-            int k = InputValidator.ReadIntNonZero("Введите k (целое, не ноль): ");
-
-            List<int> original = new List<int>();
-            List<int> filtered = new List<int>();
-
+            Console.Write("Исходные данные: ");
             using (BinaryReader reader = new BinaryReader(File.OpenRead(inputPath)))
             {
                 while (reader.BaseStream.Position < reader.BaseStream.Length)
                 {
-                    int num = reader.ReadInt32();
-                    original.Add(num);
-                    if (num % k != 0) filtered.Add(num);
+                    Console.Write(reader.ReadInt32() + " ");
                 }
             }
 
-            using (BinaryWriter writer = new BinaryWriter(File.OpenWrite(outputPath)))
+            int k = InputValidator.ReadInteger("\nВведите число k: ");
+
+            using (BinaryReader reader = new BinaryReader(File.OpenRead(inputPath)))
+            using (BinaryWriter writer = new BinaryWriter(File.Open(outputPath, FileMode.Create)))
             {
-                for (int i = 0; i < filtered.Count; i++)
-                    writer.Write(filtered[i]);
+                while (reader.BaseStream.Position < reader.BaseStream.Length)
+                {
+                    int number = reader.ReadInt32();
+                    if (number % k != 0) writer.Write(number);
+                }
             }
 
-            Console.WriteLine($"\nИсходные: {string.Join(", ", original)}");
-            Console.WriteLine($"Не кратные {k}: {(filtered.Count > 0 ? string.Join(", ", filtered) : "нет")}");
-            Console.Write("\nНажмите любую клавишу...");
-            Console.ReadKey();
+            Console.Write("Результат фильтрации: ");
+            using (BinaryReader reader = new BinaryReader(File.OpenRead(outputPath)))
+            {
+                while (reader.BaseStream.Position < reader.BaseStream.Length)
+                {
+                    Console.Write(reader.ReadInt32() + " ");
+                }
+            }
+            Console.WriteLine();
         }
 
         public static void SolveTask5()
         {
-            Console.Clear();
-            Console.WriteLine("Задание 5. Игрушки (XML), поиск для 3 лет\n");
+            string path = "toys.xml";
+            List<Toy> toysList = new List<Toy>();
 
-            string filePath = "toys.xml";
-            FillToysFile(filePath);
-            Console.WriteLine($"Файл '{filePath}' создан (XML-сериализация).\n");
+            Toy doll = new Toy(); doll.Name = "Кукла"; doll.Price = 1500; doll.MinAge = 2; doll.MaxAge = 7;
+            Toy ball = new Toy(); ball.Name = "Мяч"; ball.Price = 500; ball.MinAge = 1; ball.MaxAge = 15;
+            Toy car = new Toy(); car.Name = "Машинка"; car.Price = 200; car.MinAge = 1; car.MaxAge = 15;
+            toysList.Add(doll); toysList.Add(ball);
 
-            XmlSerializer xml = new XmlSerializer(typeof(List<Toy>));
-            List<Toy> toys;
-            using (FileStream fs = new FileStream(filePath, FileMode.Open))
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Toy>));
+            using (FileStream stream = new FileStream(path, FileMode.Create))
             {
-                toys = (List<Toy>)xml.Deserialize(fs);
+                serializer.Serialize(stream, toysList);
             }
 
-            Console.WriteLine("Все игрушки:");
-            for (int i = 0; i < toys.Count; i++)
-                Console.WriteLine($"  {toys[i].Name} — {toys[i].Price} руб. (возраст {toys[i].MinAge}-{toys[i].MaxAge})");
-
-            Console.WriteLine("\nПодходящие для 3 лет (кроме мяча):");
-            bool found = false;
-            for (int i = 0; i < toys.Count; i++)
+            Console.WriteLine("Игрушки для ребенка 3-х лет (кроме мячей):");
+            using (FileStream stream = new FileStream(path, FileMode.Open))
             {
-                Toy toy = toys[i];
-                if (toy.Name.ToLower() != "мяч" && toy.MinAge <= 3 && toy.MaxAge >= 3)
+                List<Toy> deserializedToys = (List<Toy>)serializer.Deserialize(stream);
+                foreach (Toy toy in deserializedToys)
                 {
-                    Console.WriteLine($"  {toy.Name} — {toy.Price} руб.");
-                    found = true;
+                    if (toy.Name.ToLower() != "мяч" && toy.MinAge <= 3 && toy.MaxAge >= 3)
+                    {
+                        Console.WriteLine(toy.Name + " — " + toy.Price + " руб.");
+                    }
                 }
             }
-
-            if (!found) Console.WriteLine("  не найдено");
-            Console.Write("\nНажмите любую клавишу...");
-            Console.ReadKey();
         }
-    }
-
-    public struct Toy
-    {
-        public string Name;
-        public int Price;
-        public int MinAge;
-        public int MaxAge;
     }
 }
